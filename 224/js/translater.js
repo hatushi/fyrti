5716ad06484074710fce896e6c0b1b0e668cf8ac
hatushi <64690518+hatushi@users.noreply.github.com>
2020-05-06 16:20:01 +0300

    Add files via upload

---
+++ 224/js/translater.js
@@ -0,0 +1,180 @@
+var new_lang = {
+	
+  
+  "lt0": "emagrecimento revolucionário<img alt=\"\" src=\"img/@2x/p1.png\"><span>por conta da cetose!</span>",
+  
+  "lt1": "apenas hoje!<span>desconto de 50%</span>",
+  
+  "lt2": "novo preço:<span class=\"price_main\"></span>",
+  
+  "lt3": "preço regular:<span class=\"price_old\"></span>",
+  
+  "lt4": "Até ao final da promoção:",
+  
+  "lt5": "Selecione o seu país:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt6": "Escreva o seu nome:<input name=\"name\" placeholder=\"Olga Morães\" required=\"\" type=\"text\">",
+  
+  "lt7": "Escreva o seu número de telefone:<input name=\"phone\" placeholder=\"+351 (937) 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt8": "encomendar agora",
+  
+  "lt9": "encomendar agora",
+  
+  "lt10": "Emagrecimento de forma segura<span>por conta da cetose</span>",
+  
+  "lt11": "<b>Cetose é a obtenção da energia das gorduras, e não da glicose que entra no corpo com carboidratos que consumimos.</b>Se o corpo deixar de receber a energia dos carboidratos, depois de 7 a 14 dias este entra no estado de cetose. Como resultado, começa a transformação ativa de gorduras em energia.",
+  
+  "lt12": "No entanto, é difícil tolerar uma dieta que envolva a exclusão de carboidratos completa da alimetação<br><span style=\"color: #9f116b; font-weight: 700;\"Com o KETO DIET você poderá iniciar o processo de cetose sem “torturar” o seu corpo.</span> O remédio fornecerá a ingestão de сorpos cetónicos <br>, ajudando você a livrar-se de peso em excesso.",
+  
+  "lt13": "A SUA",
+  
+  "lt14": "CETO",
+  
+  "lt15": "DIETA",
+  
+  "lt16": "keto<span>diet</span>",
+  
+  "lt17": "Componente obrigatório da dieta.",
+  
+  "lt18": "As cápsulas iniciam a cetose no seu corpo e transformam gorduras armazenadas em energia.<b> Você vai perdendo peso todos os dias de forma garantida.</b>",
+  
+  "lt19": "COMBINAÇÃO VENCEDORA<span class=\"big\">CETOSE+</span><span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt20": "Geralmente, a cetose leva de 7 a 14 dias para começar. Apenas 1 cápsula de KETO DIET reduzirá esse tempo para 40 a 50 minutos, durante o qual a conversão de gorduras em energia e a redução de gordura corporal serão iniciados. Tome <span> KETO DIET diariamente e perca até 10 a 15 kg de peso por mês!</span>", 
+  
+  "lt21": "primeira fase",
+  
+  "lt22": "A cetose permitir-lhe-á queimar gorduras de difícil acesso, preservando enquanto isso a massa muscular. A redução de consumo de carboidratos garante a estabilização do peso e o não retorno de quilogramas perdidos.",
+  
+  "lt23": "segunda fase",
+  
+  "lt24": "Keto Diet inicia a cetose: acelera o metabolismo, reduz o apetite e ataca diretamente as suas reservas de gordura!",
+  
+  "lt25": "encomendar agora",
+  
+  "lt26": "<span class=\"big\">PODEROSOS COMPONENTES</span>NA COMPOSIÇÃO<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt27": "Cetonas de framboesa",
+  
+  "lt28": "Têm uma ação semelhantes aos corpos cetónicos produzidos pelo corpo humano. São extremamente ativas.<ul><li><span>Convertem</span>a gordura em energia</li><li><span>Aceleram</span>o metabolismo</li><li><span>Diminuem</span>apetite</li></ul>",
+  
+  "lt29": "Beta sitosterol",
+  
+  "lt30": "Neutraliza os efeitos da desnutrição <ul><li><span>Normaliza</span>o metabolismo lipídico </li><li><span> Limpa os vasos sanguíneos</span>de colesterol perigoso</li><li><span>Melhora a condição da pele,</span>luta contra a celulite</li></ul>",
+  
+  "lt31": "Coenzima Q10",
+  
+  "lt32": "A deficiência de coenzima Q10 é a principal causa da obesidade em 78% dos casos <ul><li><span>Normaliza</span>o metabolismo</li><li><span>Diminui</span>o nível de colesterol perigoso </li><li><span>Diminui,</span>apetite</li></ul>",
+  
+  "lt33": "L-carnitina",
+  
+  "lt34": "Uma substância relacionada às vitaminas B, sintetizada no corpo humano <ul><li><span>Converte</span>gorduras em energia</li><li><span> Alivia a irritabilidade,</span>melhora o ânimo</li><li><span>Livra</span>da depressão</li></ul>",
+  
+  "lt35": "Bromelina",
+  
+  "lt36": "Proteína digestora de gorduras secretada na polpa e no talo do ananás<ul><li><span>Dissolve ativamente</span> a gordura visceral </li><li><span>Elimina</span>inchaços</li><li><span>Previne</span>a obesidade dos órgãos internos </li></ul>",
+  
+  "lt37": "Sinefrina orgânica",
+  
+  "lt38": "Lipolítico ativo. Bloqueia a atividade dos receptores alfa nas células adiposas <ul><li><span> Luta conta gordura persistente, incluindo a visceral </span></li><li><span>Melhora a ingestão</span>de calorias</li><li><span>Melhora a capacidade de trabalhar</span>e a resistência</li></ul>",
+  
+  "lt39": "OPINIÃO DE PERITOS SOBRE<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt40": "<span>Dorothy Goodall</span>, nutricionista norte-americana com a experiência profissional de 30 anos.",
+  
+  "lt41": "Durante a cetose, as gorduras são decompostas em ácidos graxos e glicerina, que, posteriormente, são transformadas em corpos cetónicos. No entanto, a cetose começa apenas quando as reservas de glicogênio no fígado e no tecido muscular estão completamente esgotados, <b> geralmente tal ocorre entre 7 e 14 dias.</b>",
+  
+  "lt42": "Para acelerar esse processo e, por conseguinte, a queima de gordura, recomendo aos meus pacientes o suplemento dietético KETO DIET. KETO DIET sustenta perfeitamente o corpo durante a dieta cetogénica: este enriquece o corpo com os elementos necessários, suprime a fome, elimina a fadiga e a ansiedade..<b>KETO DIET assegura uma perda de peso saudável e natural, sem causar flacidez da pele, trazendo benefícios excepcionais ao seu corpo.</b>",
+  
+  "lt43": "KETO DIET acelera a transição do corpo para o estado de cetose e ajuda na transformação de gorduras em energia.",
+  
+  "lt44": "encomendar agora",
+  
+  "lt45": "Junte-se<b>a milhares de pessoas,</b>que já experimentaram<span class=\"h3\">keto<span>diet</span></span>",
+  
+  "lt46": "Comentários",
+  
+  "lt47": "Anteriores",
+  
+  "lt48": "Sabemos tão pouco sobre o nosso corpo! Eu nunca fiquei a pensar em processos complexos que acontecem nele até chegar a uma nutricionista e ouvir sobre a cetose. Eu sou uma pessoa preguiçosa. Não consigo fazer dieta, tampouco praticar desporto. Portanto, eu estava à procura dum método fácil e rápido. Posso garantir que KETO DIET ajudará com certeza! Com este remédio, consegui perder um peso recorde de 15 kg e obter a minha silhueta ideal.",
+  
+  "lt49": "Maria P, 34 anos",
+  
+  "lt50": "Tanto quanto me lembro, eu sempre fazia dietas. Passava fome, tinha sono e problemas de pele. Às vezes conseguia perder um pouco de peso, mas depois sempre o ganhava. A minha nutricionista disse-me sobre KETO DIET. O resultado agradou quase imediatamente. Durante toda a semana eu perdia 3 quilos. Para mim foi uma verdadeira magia. Sentia-me ótima, não tinha fome, dormia 7 horas.",
+  
+  "lt51": "Amália N, 27 anos",
+  
+  "lt52": "Para mim, KETO DIET foi uma salvação. Eu nunca pensei que eu podia comer tudo o que eu gosto e, ao mesmo tempo, perder peso. É que eu sempre tive excesso de carboidratos na dieta. Por causa disso tive 100 quilos aos 29 anos de idade. Em geral, assim que eu corrigia um pouco a quantidade de carboidratos que eu consumo, o peso começava a diminuir. Mas não consigo viver sem carboidratos, porque gosto muito de fast food. Mas depois eu descobri KETO DIET. Depois de tomar o remédio, a queima de peso acelerou, eu nem precisei de desistir de carboidratos. No total, durante 4 meses, eu perdi 25 kg. KETO DIET é o melhor que já experimentei! Recomendo-o a todos!",
+  
+  "lt53": "Marta K, 49 anos",
+  
+  "lt54": "Sabemos tão pouco sobre o nosso corpo! Eu nunca fiquei a pensar em processos complexos que acontecem nele até chegar a uma nutricionista e ouvir sobre a cetose. Eu sou uma pessoa preguiçosa. Não consigo fazer dieta, tampouco praticar desporto. Portanto, eu estava à procura dum método fácil e rápido. Posso garantir que KETO DIET ajudará com certeza! Com este remédio, consegui perder um peso recorde de 15 kg e obter a minha silhueta ideal.",
+  
+  "lt55": "Maria P, 34 anos",
+  
+  "lt56": "Tanto quanto me lembro, eu sempre fazia dietas. Passava fome, tinha sono e problemas de pele. Às vezes conseguia perder um pouco de peso, mas depois sempre o ganhava. A minha nutricionista disse-me sobre KETO DIET. O resultado agradou quase imediatamente. Durante toda a semana eu perdia 3 quilos. Para mim foi uma verdadeira magia. Sentia-me ótima, não tinha fome, dormia 7 horas.",
+  
+  "lt57": "Amália N, 27 anos",
+  
+  "lt58": "Para mim, KETO DIET foi uma salvação. Eu nunca pensei que eu podia comer tudo o que eu gosto e, ao mesmo tempo, perder peso. É que eu sempre tive excesso de carboidratos na dieta. Por causa disso tive 100 quilos aos 29 anos de idade. Em geral, assim que eu corrigia um pouco a quantidade de carboidratos que eu consumo, o peso começava a diminuir. Mas não consigo viver sem carboidratos, porque gosto muito de fast food. Mas depois eu descobri KETO DIET. Depois de tomar o remédio, a queima de peso acelerou, eu nem precisei de desistir de carboidratos. No total, durante 4 meses, eu perdi 25 kg. KETO DIET é o melhor que já experimentei! Recomendo-o a todos",
+  
+  "lt59": "Marta K, 49 anos",
+  
+  "lt60": "Sabemos tão pouco sobre o nosso corpo! Eu nunca fiquei a pensar em processos complexos que acontecem nele até chegar a uma nutricionista e ouvir sobre a cetose. Eu sou uma pessoa preguiçosa. Não consigo fazer dieta, tampouco praticar desporto. Portanto, eu estava à procura dum método fácil e rápido. Posso garantir que KETO DIET ajudará com certeza! Com este remédio, consegui perder um peso recorde de 15 kg e obter a minha silhueta ideal.",
+  
+  "lt61": "Maria P, 34 anos",
+  
+  "lt62": "Seguinte",
+  
+  "lt63": "Chegou a hora<b>de livrar-se de complexos</b>",
+  
+  "lt64": "método revolucionário<img alt=\"\" src=\"img/@2x/p1.png\"><span>por conta da cetose!</span>",
+  
+  "lt65": "apenas hoje!<span>desconto de 50%</span>",
+  
+  "lt66": "novo preço:<span class=\"price_main\"></span>",
+  
+  "lt67": "preço regular:<span class=\"price_old\"></span>",
+  
+  "lt68": "Até ao final da promoção:",
+  
+  "lt69": "Selecione o seu país:<select class=\"country_select\" id=\"country\" name=\"country\"></select>",
+  
+  "lt70": "Escreva o seu nome:<input name=\"name\" placeholder=\"Olga Morães\" required=\"\" type=\"text\">",
+  
+  "lt71":  "Escreva o seu número de telefone:<input name=\"phone\" placeholder=\"+351 (937) 552-27-65\" required=\"\" type=\"text\">",
+  
+  "lt72": "encomendar agora",
+  
+  "lt73": "Como trabalhamos",
+  
+  "lt74": "FAZER PEDIDO",
+  
+  "lt75": "Logo depois de você enviar o pedido, o nosso gerente entrará em contato com você para <br><b>confirmá-lo</b>",
+  
+  "lt76": "ENTREGA",
+  
+  "lt77": "Logo depois de você enviar o pedido, o nosso gerente entrará em contato com você para <br><b>confirmá-lo</b>",
+  
+  "lt78": "PAGAMENTO",
+  
+  "lt79": "<b>Sem pré-pagamento!</b>Você paga apenas depois de receber o produto.",
+  
+  "lt80": "Garantias",
+  
+  "lt81": "Ao fazer uma compra na nossa <br>loja-online você pode ter certeza que está a adquirir um produto de alta qualidade, da alta qualidade dos produtos",
+  
+  
+	
+};
+function Translater () {
+               for (class_name in new_lang) {
+                var elements = document.getElementsByClassName(class_name);
+                if (elements.length) {
+                 for (key in elements) {
+                  elements[key].innerHTML = new_lang[class_name];
+                 }
+              }
+            }
+          };
